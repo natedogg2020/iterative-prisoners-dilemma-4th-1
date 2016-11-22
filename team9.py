b3a0c778be300b78f8876714566ad217df49e2ad
CJ-Pete <200929@c1mrxd6ph3qd.isd720.com>
2016-11-22 11:24:20 -0600

    Please work

--- team9.py
+++ team9.py
@@ -26,6 +26,11 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
+    mhis = my_history
+    thhis = their_history
+    mscore = my_score
+    thscore = their_score
+    
     return 'c'
 
     
