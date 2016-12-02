e4a558fc2b4116aca45a35de1ff6b430a10f1055
PineappleEater720 <luv2sk8board@live.com>
2016-12-02 11:05:03 -0600

    Added description

--- team5.py
+++ team5.py
@@ -8,7 +8,7 @@
 
 team_name = 'string beans' # Only 10 chars displayed.
 strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_description = 'Colludes until betrayed.'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
